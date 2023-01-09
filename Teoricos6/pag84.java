dd3c5626c98283c10a79ec2468ee75a9385440c2
Frederico Malaquias <91347722+Fredaumskrt@users.noreply.github.com>
2023-01-09 19:40:19 -0300

    Add files via upload

---
+++ Teoricos6/pag84.java
@@ -0,0 +1,11 @@
+class pag84{
+    public class void main(String[] args){
+        double c;
+        c.codigo = 5;
+        Cliente *p = NULL;
+        p = (Cliente*) malloc (sizeof(Cliente));
+        p->codigo = 6;
+        Cliente *p2 = &c;
+        p2 -> codigo = 7;
+    }
+}
